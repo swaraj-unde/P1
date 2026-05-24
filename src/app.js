eebc0ac466558a2b211ee1bc36df14a2cca3e867
Swaraj <swarajunde1@gmail.com>
2026-05-24 22:41:20 +0530

    User v1 routes

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+//routes
+import userRoutes from './routes/user.routes.js';
+
+//routes declaration
+app.use("/api/v1/users", userRoutes);
+
 export default app;
