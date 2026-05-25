aa2afc7d8150d7c43954fb0b756bfac0252f46a9
Swaraj <swarajunde1@gmail.com>
2026-05-25 11:58:38 +0530

    Testing and DeBugging

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: 'auto',
         });
-        console.log('File uploaded to Cloudinary successfully ', response.url);
+        //console.log('File uploaded to Cloudinary successfully ', response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (err) {
         fs.unlinkSync(localFilePath);
